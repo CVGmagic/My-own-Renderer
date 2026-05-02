629584b9e66a843007d055113a08d3908c13f3bd
cvgmagic <caius.grobbel@gmail.com>
2026-05-02 13:17:32 +0200

    Added saved_scenes.py, obj_classes.py and a benchmark scene

--- ray_tracer.py
+++ ray_tracer.py
@@ -5,93 +5,8 @@ import math
 from numba import njit, prange
 from object_type_flags import *
 from light_type_flags import *
+from obj_classes import *
 
-
-class Scene:
-    def __init__(self, cw, ch, vw, vh, d, O, max_rec_depth=3, rays_per_pixel=1):
-        self.objects = []
-        self.lights = []
-        self.max_rec_depth = max_rec_depth
-        self.rays_per_pixel = rays_per_pixel
-
-        """Controls image resolution"""
-        self.cw: int = cw
-        self.ch: int = ch
-        self.img = np.full((ch, cw, 3), 255, dtype=np.float64)
-
-        """Controls FOV"""
-        self.vw = vw
-        self.vh = vh
-        self.d = d
-
-        """Camera position"""
-        self.O = O
-
-        """Array for faster runtime, will be set using compile"""
-        self.obj_types = None  # Stores the type of each Object
-        self.colors = None
-        self.emitted_colors = None
-        self.emission_strengths = None
-        self.smoothnesses = None
-
-        self.sphere_centers = None
-        self.sphere_radii = None
-
-
-    def add_objects(self, *args):
-        for obj in args:
-            self.objects.append(obj)
-
-
-    def add_lights(self, *args):
-        for light in args:
-            self.lights.append(light)
-
-
-    def compile(self):
-        """Turns the object data into lists for performance improvement"""
-        """General object data"""
-        obj_types = np.zeros(len(self.objects), dtype=np.int32)
-        colors = np.zeros((len(self.objects), 3), dtype=np.float64)
-        emitted_colors = np.zeros((len(self.objects), 3), dtype=np.float64)
-        emission_strengths = np.zeros(len(self.objects), dtype=np.float64)
-        smoothnesses = np.zeros(len(self.objects), dtype=np.float64)
-
-        """Sphere data"""
-        sphere_centers = np.zeros((len(self.objects), 3))
-        sphere_radii = np.zeros(len(self.objects))
-
-        for i, obj in enumerate(self.objects):
-
-            colors[i] = obj.color
-            emitted_colors[i] = obj.emitted_color
-            emission_strengths[i] = obj.emission_strength
-            smoothnesses[i] = obj.smoothness
-
-
-            if type(obj) == Sphere:
-                obj_types[i] = SPHERE
-                sphere_radii[i] = obj.r
-                sphere_centers[i] = obj.C
-
-        self.obj_types = obj_types
-        self.colors = colors
-        self.emitted_colors = emitted_colors
-        self.emission_strengths = emission_strengths
-        self.smoothnesses = smoothnesses
-
-        self.sphere_centers = sphere_centers
-        self.sphere_radii = sphere_radii
-
-
-class Sphere:
-    def __init__(self, center: np.ndarray, radius: float, color=np.array([0, 0, 0]), emitted_color=np.array([0, 0, 0]), emission_strength=0, smoothness=0):
-        self.C = center
-        self.r = radius
-        self.color = color / 255
-        self.emitted_color = emitted_color / 255
-        self.emission_strength = emission_strength
-        self.smoothness = smoothness
 
 
 
@@ -610,7 +525,7 @@ scene = Scene(
     d = 1,
     O = np.array([0, 0, 0], dtype=np.float64),
     max_rec_depth=4,
-    rays_per_pixel=np.inf
+    rays_per_pixel=10
 )
 
 scene.add_objects(
@@ -647,7 +562,7 @@ scene.add_objects(
 
 
 #scene.img.fill(255)
-#benchmark(scene)
+benchmark(scene)
 
-render_scene_over_time(scene)
+#render_scene_over_time(scene)
 
---
+++ saved_scenes.py
@@ -0,0 +1,46 @@
+import numpy as np
+from obj_classes import *
+
+"""Initialize scene"""
+benchmark_scene = Scene(
+    cw = 450,
+    ch = 300,
+    vw = 1.5,
+    vh = 1,
+    d = 1,
+    O = np.array([0, 0, 0], dtype=np.float64),
+    max_rec_depth=4,
+    rays_per_pixel=10
+)
+
+scene.add_objects(
+    Sphere(
+            center=np.array([-8, -4, 20]),
+            radius=1,
+            color=np.array([255, 0, 0]), # Red
+           ),
+    Sphere(
+            center=np.array([10, -1, 20]),
+            radius=4,
+            color=np.array([255, 255, 255]), # White
+            smoothness=1 # reflective
+           ),
+    Sphere(
+            center=np.array([0, -2, 16]),
+            radius=2,
+            color=np.array([0, 255, 0]), # Green
+            smoothness=0
+           ),
+    Sphere(
+            center=np.array([3, -54, 20]),
+            radius=50,
+            color=np.array([255, 255, 0]), # Yellow
+        ),
+    Sphere(
+        center=np.array([-70, 40, 100]),
+        radius=80,
+        color=np.array([0, 0, 0]),
+        emitted_color=np.array([255, 255, 255]),
+        emission_strength=2 # Light source
+        )
+)
